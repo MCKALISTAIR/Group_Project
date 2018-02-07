0d581c12554eea3a2f0c24dea3df3f04efcbe622
Alistair Mckay <allymckay5@gmail.com>
2018-02-07 16:41:25 +0000

    Added header, image hovers, removed carousel controls, added coments

--- backend.py
+++ backend.py
@@ -23,8 +23,31 @@ def video():
 def uploadinstructions():
     return render_template('uploadinstructions.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
