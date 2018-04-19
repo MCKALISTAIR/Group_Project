dbfedb47500d0cef01d5d4427c973f83535799aa
Sam Reynolds <sam.3.reynolds@btinternet.com>
2018-04-19 13:30:09 +0100

    password database

--- backend.py
+++ backend.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 import json
+import MySQLdb
 from flask import Flask, flash, session, render_template, url_for, request, session, redirect, abort
 #from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
 #from flask_mail import Mail, Message
 #from flask_pymongo import PyMongo
 # import bcrypt
+#from MySQLdb import escape_string as thwart
+from dbconnect import connection
 app = Flask(__name__)
 
 app.secret_key = 'qGseyftsYb9rdYIIfz2cXjhJT9ZJwIxI8Pr0YvUd'
@@ -127,24 +130,42 @@ def logout():
 
 @app.route("/login", methods=['POST','GET'])
 def login():
-    # Check that the user supplied details in the POST
-    if request.method == 'POST':
-        if request.form['passwd'] == "":
-            flash('You must enter a password')
-            return render_template('main.html')
-        else:
+	error = ''
+        try:
+    	# Check that the user supplied details in the POST
+    	  # if request.method == 'POST':
+        	   #   if request.form['passwd'] == "":
+            	   #      flash('You must enter a password')
+            	    #     return render_template('main.html')
+        	     # else:
+
+
             #this has to change when the database gets done
-            if request.form['passwd'] == "password":
-                session['user'] = "Admin"
-                session['status'] = "admin"
+            if sha256_crypt.verify(request.form['password'], data):
+                session['logged_in'] = True
 
+                flash("You are now logged in")
                 return redirect(url_for('admin'))
-
             else:
-                flash('Wrong password fool')
-                return redirect(url_for('main'))
-    else:
-        return render_template('admin.html')
+                error = "Invalid Password, try again."
+
+            return render_template("main.html", error=error)
+        except Exception as e:
+            error = "Invalid Password, try again."
+            return render_template("main.html", error = error)
+
+
+            #if request.form['passwd'] == "password":
+             #   session['user'] = "Admin"
+              #  session['status'] = "admin"
+
+               # return redirect(url_for('admin'))
+
+            #else:
+             #  flash('Wrong password fool')
+            #return redirect(url_for("main"))
+    #else:
+        #return render_template('admin.html')
 
 
 @app.errorhandler(403)
