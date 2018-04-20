144d2e0ac2b02d6c383c40b2aacd8a5b95d13f5e
MCKALISTAIR <allymckay5@gmail.com>
2018-04-20 11:35:52 +0100

    sams databse stuff added to init file

--- __init__.py
+++ __init__.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 import json
+import MySQLdb
 from flask import Flask, flash, session, render_template, url_for, request, session, redirect, abort
 import os
 from werkzeug.utils import secure_filename
-# from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
-# from flask_mail import Mail, Message
+from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
+from flask_mail import Mail, Message
 #from flask_pymongo import PyMongo
-import bcrypt
+#import bcrypt
 app = Flask(__name__)
-# photos = UploadSet('photos', IMAGES)
+photos = UploadSet('photos', IMAGES)
 
 UPLOAD_FOLDER = 'static/testimage'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-# app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
-# configure_uploads(app, photos)
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
+configure_uploads(app, photos)
 app.secret_key = 'qGseyftsYb9rdYIIfz2cXjhJT9ZJwIxI8Pr0YvUd'
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -23,7 +24,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def storyTime(form, zipFolder):
+def storyTime(form, filename):
     storyTime= {}
     storyTime["Title"] = form["Title"]
     storyTime["ParaOne"] = form["StoryPara"]
@@ -32,7 +33,7 @@ def storyTime(form, zipFolder):
     storyTime["ParaFour"] = form["StoryParaFour"]
     storyTime["Quote"] = form["Quote"]
     storyTime["schoolSelect"] = form["schoolSelect"]
-    storyTime["zipFolder"] = zipFolder
+    storyTime["filename"] = filename
     return storyTime
 
 def videoTime(form, filename):
@@ -53,16 +54,15 @@ with open('videoStories.json') as vidin_file:
     viddata = json.load(vidin_file)
     vidin_file.close()
 
-# app.config.update(
-# 	#EMAIL SETTINGS
-# 	MAIL_SERVER='smtp.gmail.com',
-# 	MAIL_PORT=465,
-# 	MAIL_USE_SSL=True,
-# 	MAIL_USERNAME = 'napierplacementnoreply@gmail.com',
-# 	MAIL_PASSWORD = 'Placement5000'
-# 	)
-# mail = Mail(app)
-#app.config['MAIL_DEFAULT_SENDER'] = ‘allymckay5@gmail.com’
+app.config.update(
+ 	#EMAIL SETTINGS
+ 	MAIL_SERVER='smtp.gmail.com',
+ 	MAIL_PORT=465,
+ 	MAIL_USE_SSL=True,
+ 	MAIL_USERNAME = 'napierplacementnoreply@gmail.com',
+ 	MAIL_PASSWORD = 'Placement5000'
+ 	)
+mail = Mail(app)
 @app.route("/", methods=['POST','GET'])
 def main():
     return render_template('main.html', data=data, viddata=viddata)
@@ -70,33 +70,33 @@ def main():
 # def phptest():
 #     return render_template('test.html')
 #
-# @app.route('/Send/', methods=['POST','GET'])
-# def Send():
-# 		try:
-# 			#emaiil = request.form['email'] + request.form['email']
-# 			msg = Message("Placement site access request",
-# 			  sender="placmentnoreply@gmail.com",
-# 			  recipients=["allymckay5@gmail.com"])
-# 			msg.body = "Hello! This is an automated message to say that " + request.form['email'] + " has requested access to PLACEHOLDER. Please do not reply to this message, the mailbox is not monitored."
-# 			mail.send(msg)
-# 			flash('Request sent!')
-# 			return redirect(url_for('main'))
-# 		except Exception, e:
-# 			return(str(e))
+@app.route('/Send/', methods=['POST','GET'])
+def Send():
+ 		try:
+ 			emaiil = request.form['email'] + request.form['email']
+ 			msg = Message("Placement site access request",
+ 			  sender="placmentnoreply@gmail.com",
+ 			  recipients=["allymckay5@gmail.com"])
+ 			msg.body = "Hello! This is an automated message to say that " + request.form['email'] + " has requested access to PLACEHOLDER. Please do not reply to this message, the mailbox is not monitored."
+ 			mail.send(msg)
+ 			flash('Request sent!')
+ 			return redirect(url_for('main'))
+ 		except Exception, e:
+ 			return(str(e))
 
 @app.route("/textupload/", methods=['POST','GET'])
 def text():
-    if request.method == "POST" and "zipFolder" in request.files:
-        f = request.files.getlist('zipFolder')
-        for zipFile in f:
-            filename = zipFile.filename.split('/')[0]
-            zipFile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    if request.method == "POST" and "filename" in request.files:
+        file = request.files['filename']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     if request.method == "POST" and "Title" in request.form:
         data[request.form["Title"]] = storyTime(request.form, filename)
         with open('newstories.json', 'w') as outfile:
             json.dump(data, outfile)
         flash("Your Story Has Been Posted!")
-        return render_template("main.html", data=data, zipFile=zipFile)
+        return render_template("main.html", data=data, filename=filename)
     return render_template("textupload.html", data=data)
 
 @app.route("/videoupload/", methods=['POST','GET'])
@@ -182,24 +182,21 @@ def logout():
 
 @app.route("/login/", methods=['POST','GET'])
 def login():
-    # Check that the user supplied details in the POST
-    if request.method == 'POST':
-        if request.form['passwd'] == "":
-            flash('You must enter a password')
-            return render_template('main.html')
+    error = ''
+    try:
+        if sha256_crypt.verify(request.form['password'], data):
+            session['logged_in'] = True
+
+            flash("You are now logged in")
+            return redirect(url_for('admin'))
         else:
-            #this has to change when the database gets done
-            if request.form['passwd'] == "password":
-                session['user'] = "Admin"
-                session['status'] = "admin"
-
-                return redirect(url_for('admin'))
-
-            else:
-                flash('Wrong password fool')
-                return redirect(url_for('main'))
-    else:
-        return render_template('admin.html')
+            flash("Invalid Password, try again.")
+            error = "Invalid Password try again."
+        return redirect(url_for('main', error = error))
+    except Exception as e:
+        flash("Invalid Password, try again.")
+        error = "Invalid Password, try again."
+        return redirect(url_for('main', error = error))
 
 @app.route("/story/<storyTime>/", methods=['POST','GET'])
 def storyTime(storyTime):
